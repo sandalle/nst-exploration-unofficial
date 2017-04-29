74171efcb32911684da999ac21e379c507315390
Eric Sandall <sandalle@gmail.com>
2017-04-28 18:34:46 -0700

    Running server with 3G seems to work well.

--- config.sh
+++ config.sh
@@ -1,7 +1,7 @@
 #just to set some globals used by the other scripts
 FORGE='forge-1.11.2-13.20.0.2284-universal.jar'
 SERVER_DIR='/home/minecraft/multicraft/servers/mc1.11'
-START_MEMORY=5120
-MAX_MEMORY=5120
+START_MEMORY=3072
+MAX_MEMORY=3072
 
 [[ -f /etc/profile.d/java.sh ]] && . /etc/profile.d/java.sh
