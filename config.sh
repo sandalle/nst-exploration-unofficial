3075ee74133853b5c12c51e7590a4ac141259be6
Eric Sandall <sandalle@gmail.com>
2017-07-09 18:51:29 -0700

    Update Forge Mod Loader 1.11.2-13.20.1.2393

--- config.sh
+++ config.sh
@@ -1,5 +1,5 @@
 #just to set some globals used by the other scripts
-FORGE='forge-1.11.2-13.20.1.2388-universal.jar'
+FORGE='forge-1.11.2-13.20.1.2393-universal.jar'
 SERVER_DIR='/home/minecraft/multicraft/servers/mc1.11'
 START_MEMORY=3072
 MAX_MEMORY=3072
